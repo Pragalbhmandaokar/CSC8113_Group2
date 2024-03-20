4439c08d6b22b6cc7ba1cd77e3b165e3535eb40d
boyan-cn <c3010958@newcastle.ac.uk>
2024-03-20 08:50:26 +0000

    2.AgreementSmartContract

--- contracts/AgreementSmartContract.sol
+++ contracts/AgreementSmartContract.sol
@@ -6,26 +6,65 @@ import "./DataUsageSmartContract.sol";
 
 contract AgreementSmartContract is AccessControl {
 
+    // Struct
+    
     struct Consent {
-        uint dataUsageId;             // ID linking to a specific DataUsage record
-        address purposeBlockAddress;  // Address of the DataUsageSmartContract
-        uint userId;                  // ID of the user giving consent
-        string servicePurpose;        // The purpose for which the service is used
-        bool isConsent;               // Whether negative(false) / positive(true)
+        address purposeBlockAddress;        // = dataUsageSmartContractAddress, input
+        uint userId;                        // input
+        uint dataUsageId;                   // can get the dataUsage, input
+        bool isConsented;                   // positive == true / negative == false, input
+        string servicePurpose;              // retrieve from this dataUsage
     }
 
-    DataUsageSmartContract public dataUsageSmartContract;
+    // Associate with the DataUsageSmartContract
 
-    mapping(uint => Consent) private consents;       // Mapping from userID to consent
-    uint private consentCount = 0;
+    DataUsageSmartContract private dataUsageSmartContract;
 
-    event ConsentAdded(uint indexed userId, uint indexed dataUsageId, bool isConsent);
+    // Mapping
 
+    mapping(uint => Consent) private consents;          // mapping consents <uint dataUsageId, Consent theConsent>
+    uint[] private consentKeys;                         // key  = Consent.dataUsageId(uint)
+    uint private consentsCounter = 0;                   // counter,(start from 0, ++ when add)
+
+    // Event
+    event ConsentAdded(uint dataUsageId);
+
+    // Constructor
     constructor(address _dataUsageSmartContractAddress) {
         dataUsageSmartContract = DataUsageSmartContract(_dataUsageSmartContractAddress);
+        require(_dataUsageSmartContractAddress != address(0), "The DataUsageSmartContract Address does not exist.");
     }
 
+    // Function
+    function addConsent(
+        address _purposeBlockAddress,
+        uint _userId,
+        uint _dataUsageId,
+        bool _isConsented
+    ) public onlyOwner {
+        // Retrieve service purpose from data usage smart contract
+        DataUsageSmartContract.DataUsage memory dataUsage = dataUsageSmartContract.getDataUsageByKey(_dataUsageId);
+        
+        consents[_dataUsageId] = Consent({
+            purposeBlockAddress: _purposeBlockAddress,
+            userId: _userId,
+            dataUsageId: _dataUsageId,
+            isConsented: _isConsented,
+            servicePurpose: dataUsage.servicePurpose
+        });
+        
+        consentKeys.push(_dataUsageId);
+        consentsCounter++;
 
-    
+        emit ConsentAdded(_dataUsageId);
+    }
 
+    function getConsentByKey(uint _dataUsageId) public view returns (Consent memory) {
+        require(consents[_dataUsageId].dataUsageId != 0, "Consent does not exist.");
+        return consents[_dataUsageId];
+    }
+
+    function getConsentCounter() public view returns (uint) {
+        return consentsCounter;
+    }
 }
