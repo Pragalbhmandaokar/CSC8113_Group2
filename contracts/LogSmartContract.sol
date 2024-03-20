928545244648fb8c9e2ecbefce4677375c0b9412
boyan-cn <c3010958@newcastle.ac.uk>
2024-03-20 09:27:54 +0000

    3.LogSmartContract

--- contracts/LogSmartContract.sol
+++ contracts/LogSmartContract.sol
@@ -2,8 +2,73 @@
 pragma solidity ^0.8.10;
 
 import "./AccessControl.sol";
+import "./DataUsageSmartContract.sol";
 
 contract LogSmartContract is AccessControl {
 
-    
+    // Struct
+
+    struct Log {
+        uint dataUsageId;                                // can get the specific dataUsage, input
+        uint actorId;                                    // below all data get from this dataUsage
+        string[] operations;
+        string serviceName;
+        bytes32[] processedPersonalDatas;
+    }
+
+    // Associate with the DataUsageSmartContract
+
+    DataUsageSmartContract private dataUsageSmartContract;
+
+    // Mapping
+
+    mapping(uint => Log) private logs;                  // mapping logs <uint dataUsageId, Log theLog>
+    uint[] private logKeys;                             // key  = Log.dataUsageId(uint)
+    uint private logCounter = 0;                        // counter,(start from 0, ++ when add)
+
+    // Event
+    event LogAdded(uint dataUsageId);
+
+    // Constructor
+    constructor(address _dataUsageSmartContractAddress) {
+        dataUsageSmartContract = DataUsageSmartContract(_dataUsageSmartContractAddress);
+        require(_dataUsageSmartContractAddress != address(0), "DataUsageSmartContract address is invalid.");
+    }
+
+    // Function
+
+    function addLog(uint _dataUsageId) public onlyOwner {
+        // Retrieve the associated DataUsage record to ensure it exists
+        DataUsageSmartContract.DataUsage memory dataUsage = dataUsageSmartContract.getDataUsageByKey(_dataUsageId);
+
+        // Initialize processedPersonalDatas array for the log, assuming DataUsage contract provides a method to get processed data
+        bytes32[] memory processedDatas = new bytes32[](dataUsage.personalDataIds.length);
+        for(uint i = 0; i < dataUsage.personalDataIds.length; ++i) {
+            processedDatas[i] = dataUsageSmartContract.getProcessedPersonalDataByKey(dataUsage.personalDataIds[i]);
+        }
+
+        // Create the log record
+        logs[_dataUsageId] = Log({
+            dataUsageId: _dataUsageId,
+            actorId: dataUsage.actorId,
+            operations: dataUsage.operations,
+            serviceName: dataUsage.serviceName,
+            processedPersonalDatas: processedDatas
+        });
+
+        logKeys.push(_dataUsageId);
+        logCounter++;
+
+        emit LogAdded(_dataUsageId);
+    }
+
+    function getLogByKey(uint _dataUsageId) public view returns (Log memory) {
+        require(logs[_dataUsageId].dataUsageId != 0, "Log does not exist.");
+        return logs[_dataUsageId];
+    }
+
+    function getLogCounter() public view returns (uint) {
+        return logCounter;
+    }
 }
+ 
--- contracts/Verification.sol
+++ contracts/Verification.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.10;
 
 import "./AccessControl.sol";
+import "./DataUsageSmartContract.sol";
+import "./AgreementSmartContract.sol";
+import "./LogSmartContract.sol";
 
 contract Verification is AccessControl {
 
