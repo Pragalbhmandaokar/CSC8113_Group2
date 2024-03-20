462acdaaa73f6427a468dad152d7fbb6cb8ae7d1
boyan-cn <c3010958@newcastle.ac.uk>
2024-03-20 09:46:51 +0000

    Verification-framework update

--- contracts/Verification.sol
+++ contracts/Verification.sol
@@ -8,6 +8,46 @@ import "./LogSmartContract.sol";
 
 contract Verification is AccessControl {
 
+    DataUsageSmartContract private dataUsageSmartContract;
+    AgreementSmartContract private agreementSmartContract;
+    LogSmartContract private logSmartContract;
+
+    uint[] private violatorActorIds;                                   // Array to store the IDs of the actors that violated the agreements
+
+    constructor(
+        address _dataUsageSmartContractAddress,
+        address _agreementSmartContractAddress,
+        address _logSmartContractAddress
+    ) {
+        require(_dataUsageSmartContractAddress != address(0), "Invalid DataUsageSmartContract address.");
+        require(_agreementSmartContractAddress != address(0), "Invalid AgreementSmartContract address.");
+        require(_logSmartContractAddress != address(0), "Invalid LogSmartContract address.");
+
+        dataUsageSmartContract = DataUsageSmartContract(_dataUsageSmartContractAddress);
+        agreementSmartContract = AgreementSmartContract(_agreementSmartContractAddress);
+        logSmartContract = LogSmartContract(_logSmartContractAddress);
+    }
     
+
+    function verifyCompliance() public onlyOwner {
+        uint dataUsageCount = dataUsageSmartContract.getDataUsageCounter();
+        
+        for (uint i = 0; i < dataUsageCount; i++) {
+            // Assume the existence of a method in LogSmartContract to get logs by dataUsageId
+            LogSmartContract.Log memory log = logSmartContract.getLogByKey(i);
+            // Assume the existence of a method in AgreementSmartContract to get consent by dataUsageId
+            AgreementSmartContract.Consent memory consent = agreementSmartContract.getConsentByKey(i);
+
+            // Verification logic
+            if(consent.isConsented == false || log.actorId != consent.userId) {
+                // If consent is not given or log's actorId does not match consent's userId, flag as violator
+                violatorActorIds.push(log.actorId);
+            }
+        }
+    }
+
+    function getViolators() public view returns (uint[] memory) {
+        return violatorActorIds;
+    }
 }
 
